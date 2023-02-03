0d046c90bf2b6618e2d933f6d8f59f8817018093
CaolanCode <cmacmahon3@gmail.com>
2023-02-03 06:36:01 +0000

    update pointers

--- pointers.py
+++ pointers.py
@@ -1,3 +1,4 @@
+# integers are emutable
 num1 = 11
 num2 = num1
 
@@ -17,3 +18,24 @@ print("num2 = ", num2)
 print("\nnum1 points to: ", id(num1))
 print("\nnum2 points to: ", id(num2))
 
+# dictionaries can be changed where they point to
+# the unlinked memory will be taken care of with garbage collection
+dict1 =  {'value' : 11}
+
+dict2 = dict1
+
+print("Before dict2 is updated")
+print("dict1 = ", dict1)
+print("dict2 = ", dict2)
+
+print("\ndict1 points to: ", id(dict1))
+print("\ndict2 points to: ", id(dict2))
+
+dict2['value'] = 22
+
+print("After dict2 is updated")
+print("dict1 = ", dict1)
+print("dict2 = ", dict2)
+
+print("\ndict1 points to: ", id(dict1))
+print("\ndict2 points to: ", id(dict2))
