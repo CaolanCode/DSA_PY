4cd8aa5b436674a5c7f8180b3c1767c0a77f3891
CaolanCode <cmacmahon3@gmail.com>
2023-02-03 07:04:57 +0000

    update linked list

--- linked_list.py
+++ linked_list.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, value):
         self.value = value
-        self.next = value
+        self.next = None
 
 
 class LinkedList:
@@ -11,14 +11,27 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+        
+
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
+    
 
-my_linked_list = LinkedList(4)
 
-print(my_linked_list.head.value)
-print(my_linked_list.tail.value)
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.print_list()
