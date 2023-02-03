56472923ff87e391d2b7027b2336bdd58fb6a877
CaolanCode <cmacmahon3@gmail.com>
2023-02-03 11:59:38 +0000

    final commit of queue class

--- queue_class.py
+++ queue_class.py
@@ -10,6 +10,31 @@ class Queue:
         self.last = new_node
         self.length = 1
 
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+        
+        
+
     def print_queue(self):
         temp = self.first
         while temp:
@@ -18,5 +43,7 @@ class Queue:
 
 
 my_queue = Queue(1)
+my_queue.enqueue(2)
+my_queue.dequeue()
 my_queue.print_queue()
 
