2fc36ff4932e2b692ac668e51d290cc3e530a662
CaolanCode <cmacmahon3@gmail.com>
2023-02-03 07:18:37 +0000

    update linked list

--- linked_list.py
+++ linked_list.py
@@ -21,7 +21,22 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
-        
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        pre = self.head
+        temp = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
     def print_list(self):
         temp = self.head
@@ -34,4 +49,6 @@ class LinkedList:
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
+my_linked_list.print_list()
+my_linked_list.pop()
 my_linked_list.print_list()
